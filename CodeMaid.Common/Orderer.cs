1de21e72340f9f739e4aa1f0383f25cc4e0c88fc
Adrien Padol <apadol@ikomobi.com>
2017-01-16 18:19:38 +0100

    Added ordering statics, and unit tests

--- CodeMaid.Common/Orderer.cs
+++ CodeMaid.Common/Orderer.cs
@@ -11,11 +11,71 @@ namespace CodeCleaner.Common.Ordering
         {
         }
 
+        public IList<ISymbol> OrderAll(IEnumerable<ISymbol> symbols)
+        {
+            var result = new List<ISymbol>();
+
+            result.AddRange(this.OrderStaticMembers(symbols));
+            result.AddRange(this.OrderConstants(symbols));
+            //result.AddRange(this.OrderInstanceVariables(symbols));
+            //result.AddRange(this.OrderConstructor(symbols));
+            result.AddRange(this.OrderProperties(symbols));
+            result.AddRange(this.OrderMethods(symbols));
+
+            return result;
+        }
+
+        public IList<ISymbol> OrderInstanceVariables(IEnumerable<ISymbol> symbols)
+        {
+            var fields = symbols.Where(s => s is IFieldSymbol);
+
+            return OrderSymbols(fields);
+        }
+
         public IList<ISymbol> OrderConstants(IEnumerable<ISymbol> symbols)
         {
             var constants = symbols.Where(m => m is IFieldSymbol &&
                                           ((IFieldSymbol)m).IsConst);
             return OrderSymbols(constants);
+        }
+
+        public IList<ISymbol> OrderStaticMembers(IEnumerable<ISymbol> symbols)
+        {
+            List<ISymbol> result = new List<ISymbol>();
+            var staticFields = symbols.Where(m => m is IFieldSymbol &&
+                                             ((IFieldSymbol)m).IsDefinition &&
+                                             ((IFieldSymbol)m).IsStatic &&
+                                            !((IFieldSymbol)m).IsConst);
+
+            if (staticFields.Any())
+            {
+                result.AddRange(OrderSymbols(staticFields));
+            }
+
+            var staticMethods = symbols.Where(m => m is IMethodSymbol &&
+                                              ((IMethodSymbol)m).IsStatic);
+
+            if (staticMethods.Any())
+            {
+                result.AddRange(OrderSymbols(staticMethods));
+            }
+
+            return result;
+        }
+
+        public IList<ISymbol> OrderMethods(IEnumerable<ISymbol> symbols)
+        {
+            var methods = symbols.Where(m => m is IMethodSymbol &&
+                                        ((IMethodSymbol)m).MethodKind == MethodKind.Ordinary);
+
+            return OrderSymbols(methods);
+        }
+
+        public IList<ISymbol> OrderProperties(IEnumerable<ISymbol> symbols)
+        {
+            var methods = symbols.Where(m => m is IPropertySymbol);
+
+            return OrderSymbols(methods);
         }
 
         private IList<ISymbol> OrderSymbols(IEnumerable<ISymbol> constants)
--- CodeMaid.Tests/OrderingTests.cs
+++ CodeMaid.Tests/OrderingTests.cs
@@ -19,6 +19,8 @@ namespace testnm
 {
 class TestClass 
 {
+	protected const int A = 2;
+	protected const int Protected2 = 1;
 	protected const int Protected = 0;
 	private const int Private = 1;
 	public const int Public = 2;    
@@ -29,8 +31,107 @@ class TestClass
             var ordered = orderer.OrderConstants(this.GetMembersFromTree(tree));
 
             Assert.AreEqual("Public", ordered[0].Name);
-            Assert.AreEqual("Protected", ordered[1].Name);
-            Assert.AreEqual("Private", ordered[2].Name);
+            Assert.AreEqual("A", ordered[1].Name);
+            Assert.AreEqual("Protected", ordered[2].Name);
+            Assert.AreEqual("Protected2", ordered[3].Name);
+            Assert.AreEqual("Private", ordered[4].Name);
+        }
+
+        [Test]
+        public void OrderMethodsTest()
+        {
+            var tree = CSharpSyntaxTree.ParseText(@"
+namespace test{
+	class testclass{
+	
+private int A(){}
+private int PrivateMethod(){}
+public string Z(){}
+public string PublicMethod(){}
+protected object ProtectedMethod(){}
+
+}
+}            
+");
+
+            var orderer = new Orderer();
+            var ordered = orderer.OrderMethods(this.GetMembersFromTree(tree));
+
+            Assert.AreEqual("PublicMethod", ordered[0].Name);
+            Assert.AreEqual("Z", ordered[1].Name);
+            Assert.AreEqual("ProtectedMethod", ordered[2].Name);
+            Assert.AreEqual("A", ordered[3].Name);
+            Assert.AreEqual("PrivateMethod", ordered[4].Name);
+        }
+
+        [Test]
+        public void OrderPropertiesTest()
+        {
+            var tree = CSharpSyntaxTree.ParseText(@"
+namespace test{
+    class testclass{
+    
+private int D {get; private set;}
+private int C {get;set;}
+public string A{get;private set;}
+protected object B{get;}
+
+}
+}            
+");
+
+            var orderer = new Orderer();
+            var ordered = orderer.OrderProperties(this.GetMembersFromTree(tree));
+
+            Assert.AreEqual("A", ordered[0].Name);
+            Assert.AreEqual("B", ordered[1].Name);
+            Assert.AreEqual("C", ordered[2].Name);
+            Assert.AreEqual("D", ordered[3].Name);
+        }
+
+        [Test]
+        public void OrderInstanceVariablesTest()
+        {
+            var tree = CSharpSyntaxTree.ParseText(@"
+namespace test{
+    class testclass{
+    
+private int C = 2;
+string B = 1;
+public object A;
+}
+}            
+");
+
+            var orderer = new Orderer();
+            var ordered = orderer.OrderInstanceVariables(this.GetMembersFromTree(tree));
+
+            Assert.AreEqual("A", ordered[0].Name);
+            Assert.AreEqual("B", ordered[1].Name);
+            Assert.AreEqual("C", ordered[2].Name);
+
+        }
+
+        [Test]
+        public void OrderAllTest()
+        {
+            var tree = CSharpSyntaxTree.ParseText(@"
+namespace test{
+    class testclass{
+    
+private void C(){}
+protected const int A = 0;
+protected object B{get;}
+}
+}            
+");
+
+            var orderer = new Orderer();
+            var ordered = orderer.OrderAll(this.GetMembersFromTree(tree));
+
+            Assert.AreEqual("A", ordered[0].Name);
+            Assert.AreEqual("B", ordered[1].Name);
+            Assert.AreEqual("C", ordered[2].Name);
         }
 
         private ImmutableArray<ISymbol> GetMembersFromTree(SyntaxTree csharpCode)
