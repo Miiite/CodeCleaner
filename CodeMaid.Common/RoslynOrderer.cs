5454bf07379d446d7fb2fe7dd49b5ca7a3dcfd53
Adrien Padol <adrien.padol@orange.com>
2019-03-20 15:48:37 +0100

    Ajout du RoslynOrderer

---
+++ CodeMaid.Common/RoslynOrderer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.CodeAnalysis;
+using System.Linq;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace CodeMaid.Common
+{
+    public class RoslynOrderer : ICleanerStep, ISyntaxNodeCleanerStep
+    {
+        public SyntaxNode Run(SyntaxNode classNode) => this.OrderNode(classNode);
+
+        private SyntaxNode OrderNode(SyntaxNode classNode)
+        {
+            var result = this.OrderMethods(classNode);
+
+            return result;
+        }
+
+        private SyntaxNode OrderMethods(SyntaxNode classNode)
+        {
+            var methods = classNode.DescendantNodes()
+                .OfType<MethodDeclarationSyntax>();
+
+            classNode = classNode.TrackNodes(methods);
+
+            var orderedMethods = methods.OrderBy(m => m.Identifier)
+                .ToList();
+
+            foreach (var m in orderedMethods)
+            {
+                classNode = classNode.RemoveNode(m, SyntaxRemoveOptions.KeepNoTrivia);
+            }
+
+            var classRoot = classNode.ChildNodes().First();
+            classNode = classNode.InsertNodesAfter(classRoot, orderedMethods);
+
+            return classNode;
+        }
+    }
+}
